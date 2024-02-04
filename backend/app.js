d56c34603bf2ac83866eb5170ac0fef71dd72fe1
Ivan <racioglobus@yandex.ru>
2024-02-04 18:50:20 +0300

    add cors support3, p15

--- backend/app.js
+++ backend/app.js
@@ -4,35 +4,14 @@ import helmet from 'helmet';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import router from './routes/index.js';
-// import allowedCors from '../utils/const.js';
 
 const { PORT = 3000 } = process.env;
 const BASE_URL_DB = 'mongodb://localhost:27017/mestodb';
-// const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
 
 const app = express();
 
 app.use(cors());
 app.use(helmet());
-
-// app.use('*', (req, res, next) => {
-//   const { origin } = req.headers;
-//   const { method } = req;
-//   const requestHeaders = req.headers['access-control-request-headers'];
-
-//   if (allowedCors.includes(origin)) {
-//     res.header('Access-Control-Allow-Origin', origin);
-//   }
-//   if (method === 'OPTIONS') {
-//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-//   }
-//   if (method === 'OPTIONS') {
-//     res.header('Access-Control-Allow-Headers', requestHeaders);
-//     return res.end();
-//   }
-//   return next();
-// });
-
 app.use(json());
 
 async function startApp() {
--- backend/utils/const.js
+++ backend/utils/const.js
@@ -1,10 +1,3 @@
 const URLExp = /^(https?:\/\/)(www\.)?([a-zA-Z0-9-]+\.){1,}([a-zA-Z]+)(\/[-a-zA-Z0-9._~:?#[\]@!$&'()*+,;=]*)*\/?#?$/i;
 
-const allowedCors = [
-  'http://bladerunner.nomoredomainsmonster.ru/',
-  'https://bladerunner.nomoredomainsmonster.ru/',
-  'localhost:3000',
-  'localhost:5173',
-];
-
-export default { URLExp, allowedCors };
+export default { URLExp };
